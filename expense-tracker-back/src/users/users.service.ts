d3a732ff7882ac5a1ca9627bd41eae9ed77d8c41
maria-hambardzumian <maria_hambardzumyan@gmail.com>
2026-03-29 23:19:34 +0400

    Add users module

---
+++ expense-tracker-back/src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import type { Prisma } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  findById(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  findByUsername(username: string) {
+    return this.prisma.user.findUnique({ where: { username } });
+  }
+
+  create(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+}
